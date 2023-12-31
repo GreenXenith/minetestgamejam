970802fa7c541898b83fb7225abe2582d301abda
GreenXenith <24834740+GreenXenith@users.noreply.github.com>
2023-12-31 01:18:07 -0800

    Fix logging date

--- site/server/main.js
+++ site/server/main.js
@@ -6,12 +6,7 @@ const JAM_TAG = "jam_game_2023";
 const fs = require("fs");
 
 const log_msg = (message) => {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes().toString().padStart(2, "0");
-    const seconds = now.getSeconds().toString().padStart(2, "0");
-
-    const stamped_msg = `[${hours}:${minutes}:${seconds}]: ${message}\n\n`;
+    const stamped_msg = `[${(new Date()).toISOString()}]: ${message}\n\n`;
     fs.appendFile("log.txt", stamped_msg, () => {});
 
     return stamped_msg;
